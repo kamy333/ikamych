86bed51faa21f55ab539d9ca9ededc8b27b43147
Kamran <nafisspour@bluewin.ch>
2026-05-24 13:16:01 +0200

    Make missing autoload classes non-fatal

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -68,9 +68,9 @@ function my_autoloader($class_name)
     $path = LIB_PATH . DS . "{$class_name}.php";
     if (file_exists($path)) {
         require_once($path);
-    } else {
-        die("The file {$class_name}.php could not be found.");
     }
+
+    return false;
 }
 
 spl_autoload_register('my_autoloader');
